c1a5700d7585a23355421390ac3199f73ecf8748
Miis <amzdybek@gmail.com>
2020-10-10 23:21:21 +0200

    test

--- dist/src.62c16509.js
+++ dist/src.62c16509.js
@@ -28377,7 +28377,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61245" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61446" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
