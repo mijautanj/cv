3afee9fbbd936a53fe4039f33a545e900c579072
Mia Zdybek <55348731+mijautanj@users.noreply.github.com>
2020-10-12 20:04:07 +0200

    Profile (#18)
    * added picture
    
    * profile picture
    
    * centered page

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -3,7 +3,10 @@ import React from "react"
 
 export const Home: React.FC = () =>{
   return (<div>
-    <h1>Miis hemsida</h1>
-    <h2>Hej och välkommen!!!</h2>
+    <h1>Mia Zdybek</h1>
+    <img src={ require('../../../pictures/mia-bild.jpg')} width="300" height="300" />
+
+    <h2>Student at KTH (Master's programme in Machine Learing)</h2>
+   
     </div>)
 }
