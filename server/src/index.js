1b163909642992f7ccde4ca0e768621a8b14a685
Miis <amzdybek@gmail.com>
2020-11-30 15:50:18 +0100

    -

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,12 @@
-const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
- //HEÖLPP
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!!!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
